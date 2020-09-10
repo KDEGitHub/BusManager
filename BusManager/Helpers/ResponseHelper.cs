1b3cbf9582d6d1a4ffa3ff61f8a21ced7b0776a4
Дмитрий Каблуков <d.kablukov@otlnal.ru>
2020-09-10 11:11:22 +0700

    add execute functions

---
+++ BusManager/Helpers/ResponseHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BusManager.Messages;
+using Microsoft.Extensions.Logging;
+
+namespace BusManager.Helpers
+{
+    /// <summary>
+    /// класс для извлечения ответа для обработки 
+    /// </summary>
+    public static class ResponseHelper
+    {
+        public static IBusMessage TryExecuteOperationWrapper<T>(IBusMessage item, Func<T, object> operation)
+        {
+            IBusMessage message = new ResponseMessage(item.Id, item.MessageType);
+            try
+            {
+                T value = item.GetBody<T>();
+                message.Body = operation(value);
+            }
+            catch (Exception ex)
+            {
+                message.IsError = true;
+                message.ErrorDetails = $"Error : {ex.Message}";
+            }
+            return message;
+        }
+        public static async Task<IBusMessage> TryExecuteOperationWrapperAsync<T>(IBusMessage item, Func<T, CancellationToken, Task<object>> operation)
+        {
+            IBusMessage message = new ResponseMessage(item.Id, item.MessageType);
+            try
+            {
+                CancellationTokenSource cts = TokenHelper.GetTokenSource(item.CreateDate, item.TTL, DateTime.UtcNow);
+
+                T value = item.GetBody<T>();
+                message.Body = await operation(value, cts.Token);
+
+            }
+            catch (Exception ex)
+            {
+                message.IsError = true;
+                message.ErrorDetails = $"Error : {ex.Message}";
+            
+            }
+            return message;
+
+        }
+
+    }
+}
---
+++ BusManager/Messages/ResponseMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusManager.Messages
+{
+    /// <summary>
+    /// Класс реализует ответное сообщение для шины данных
+    /// </summary>
+    public class ResponseMessage : BusMessage
+    {
+        /// <summary>
+        /// Создает экземпляр из идентификатора и типа сообщения
+        /// </summary>
+        /// <param name="id">идентификатор</param>
+        /// <param name="messageType">Тип сообщения</param>
+        public ResponseMessage(Guid id, string messageType, int ttl = 30)
+        {
+            Id = id;
+            MessageType = messageType;
+            CreateDate = DateTime.UtcNow;
+            TTL = ttl;
+        }
+    }
+}
